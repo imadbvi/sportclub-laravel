0f2591d995fdeb89acfa344e6a44b7a14e312b77
imadbvi <imad.ben.ali.idrissi@student.ehb.be>
2025-12-12 15:13:27 +0100

    FAQ link toegevoegd aan navigatie

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
+                    <x-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.index')">
+    FAQ
+</x-nav-link>
                     @auth
                         @if(auth()->user()->is_admin)
                             <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
@@ -84,6 +87,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.index')">
+    FAQ
+</x-responsive-nav-link>
 
             @auth
                 @if(auth()->user()->is_admin)
