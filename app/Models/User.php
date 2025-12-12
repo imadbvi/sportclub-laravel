aa102423475294c378715fd4ddda74ac13149a7a
imadbvi <imad.ben.ali.idrissi@student.ehb.be>
2025-12-12 17:51:44 +0100

    Team-model toegevoegd met many-to-many relatie met users

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'about_me',
         'is_admin',
     ];
-    
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -50,5 +50,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function teams(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Team::class);
     }
 }
