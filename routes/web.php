931946fa7de3b9570eb8a195d07b96e7c5490ff6
imadbvi <imad.ben.ali.idrissi@student.ehb.be>
2025-12-15 14:56:27 +0100

    registratie route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/', function () {
 
 // ==== GEBRUIKERS MIDDLEWARE ====
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
